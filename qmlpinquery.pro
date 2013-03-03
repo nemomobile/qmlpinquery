0726ea955ac2631cbdddabe6f8ec46a3a1db6372
Robin Burchell <robin+git@viroteck.net>
2013-03-03 01:51:27 +0000

    Rewrite of a fair bit of the C++ and buildsystem.
    Now looks like most other Nemo applications.

--- qmlpinquery.pro
+++ qmlpinquery.pro
@@ -1,17 +1,52 @@
-# Folders to ship with the application
-folder_01.source = qml/qmlpinquery
-folder_01.target = qml
-DEPLOYMENTFOLDERS = folder_01
+PROJECT_NAME = qmlpinquery
+TEMPLATE = app
+CONFIG += ordered hide_symbols
+QT += declarative dbus
+TARGET = $$PROJECT_NAME
+CONFIG -= app_bundle # OS X
 
-# Additional import path used to resolve QML modules in Creator's code model
-#QML_IMPORT_PATH =
+CONFIG += link_pkgconfig
 
-# Define QMLJSDEBUGGER to allow debugging of QML in debug builds
-# (This might significantly increase build time)
-# DEFINES += QMLJSDEBUGGER
+packagesExist(qdeclarative-boostable) {
+    message("Building with qdeclarative-boostable support")
+    DEFINES += HAS_BOOSTER
+    PKGCONFIG += qdeclarative-boostable
+} else {
+    warning("qdeclarative-boostable not available; startup times will be slower")
+}
+PKGCONFIG += ofono-qt
 
-include(src/src.pri)
+SOURCES += src/main.cpp \
+           src/ofonosimif.cpp
 
-# Required for deployment.
-include(qmlapplicationviewer/qmlapplicationviewer.pri)
-qtcAddDeployment()
+HEADERS += src/ofonosimif.h
+RESOURCES += res.qrc
+
+QML_FILES = qml/*.qml
+JS_FILES = *qml/.js
+
+OTHER_FILES += $${QML_FILES} $${JS_FILES}
+
+target.path = $$INSTALL_ROOT/usr/bin/
+INSTALLS += target
+
+desktop.files = $${PROJECT_NAME}.desktop
+desktop.path = $$INSTALL_ROOT/usr/share/applications
+INSTALLS += desktop
+
+# qml API we provide
+qml_api.files = qml/api/*
+qml_api.path = $$[QT_INSTALL_IMPORTS]/org/nemomobile/$$PROJECT_NAME
+INSTALLS += qml_api
+
+TRANSLATIONS += *.qml
+dist.commands += rm -fR $${PROJECT_NAME}-$${VERSION} &&
+dist.commands += git clone . $${PROJECT_NAME}-$${VERSION} &&
+dist.commands += rm -fR $${PROJECT_NAME}-$${VERSION}/.git &&
+dist.commands += rm -f $${PROJECT_NAME}-$${VERSION}/.gitignore &&
+dist.commands += mkdir -p $${PROJECT_NAME}-$${VERSION}/ts &&
+dist.commands += lupdate $${TRANSLATIONS} -ts $${PROJECT_NAME}-$${VERSION}/ts/$${PROJECT_NAME}.ts &&
+dist.commands += tar jcpvf $${PROJECT_NAME}-$${VERSION}.tar.bz2 $${PROJECT_NAME}-$${VERSION} &&
+dist.commands += rm -fR $${PROJECT_NAME}-$${VERSION}
+QMAKE_EXTRA_TARGETS += dist
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,99 @@
 /*
-* This file is part of meego-pinquery
-*
-* Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies). All rights reserved.
-*
-* Contact: Sirpa Kemppainen <sirpa.h.kemppainen@nokia.com>
-*
-* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-*
-* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-* Neither the name of Nokia Corporation nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-* THE POSSIBILITY OF SUCH DAMAGE.
-*
-*/
+ * Copyright (C) 2013 Robin Burchell <robin+mer@viroteck.net>
+ * Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+ *
+ * You may use this file under the terms of the BSD license as follows:
+ *
+ * "Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Nemo Mobile nor the names of its contributors
+ *     may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+ */
 
 #include <QtGui/QApplication>
 #include <QtDeclarative/QDeclarativeComponent>
 #include <QtDeclarative/QDeclarativeContext>
 #include <QtDeclarative/QDeclarativeEngine>
+#include <QtDeclarative/QDeclarativeView>
 #include <QEvent>
 #include <QKeyEvent>
+#include <QDebug>
 
 // Header below is missing from MeeGo OBS
 //#include <QSystemScreenSaver>
 
-#include "qmlapplicationviewer.h"
 #include "ofonosimif.h"
 
-//QTM_USE_NAMESPACE
+#ifdef HAS_BOOSTER
+#include <applauncherd/MDeclarativeCache>
+#endif
 
-int main(int argc, char *argv[])
+#ifdef HAS_BOOSTER
+Q_DECL_EXPORT
+#endif
+int main(int argc, char **argv)
 {
-    QApplication app(argc, argv);
-    QmlApplicationViewer viewer;
-    bool startedFromDesktop(false);
-
-    if (argc==2){
-        if (argv[1] == QString("-desktop")){
-            startedFromDesktop = true;
-        }
-    }
-
-    QDeclarativeContext *context = new QDeclarativeContext(viewer.rootContext());
-
-    if (context->parentContext()) {
-        context = context->parentContext();
-    }
+    QApplication *application;
+    QDeclarativeView *view;
+#ifdef HAS_BOOSTER
+    application = MDeclarativeCache::qApplication(argc, argv);
+    view = MDeclarativeCache::qDeclarativeView();
+#else
+    qWarning() << Q_FUNC_INFO << "Warning! Running without booster. This may be a bit slower.";
+    QApplication stackApp(argc, argv);
+    QDeclarativeView stackView;
+    application = &stackApp;
+    view = &stackView;
+#endif
 
     OfonoSimIf *ofonoSimIf = new OfonoSimIf();
-    context->setContextProperty("ofonoSimIf", ofonoSimIf);
-    view->setSource(QUrl("qml/qmlpinquery/main.qml"));
 
-    if (ofonoSimIf->pinRequired() || startedFromDesktop) {
-        viewer.setResizeMode(QDeclarativeView::SizeRootObjectToView);
-        viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
-        viewer.setMainQmlFile(QLatin1String("qml/meego-pinquery/main.qml"));
-
-        // QSystemScreenSaver is not available in MeeGo OBS
-        //QSystemScreenSaver *screenSaver = new QSystemScreenSaver();
-        //screenSaver->setScreenSaverInhibit();
-
-        viewer.showExpanded();
-
-        // Send fake key presses to try to dismiss screensaver if it is alredy active.
-        // Don't delete events in this method
-        QKeyEvent* keyPress = new QKeyEvent( QEvent::KeyPress, Qt::Key_Space, Qt::NoModifier);
-        QKeyEvent* keyRelease = new QKeyEvent( QEvent::KeyRelease, Qt::Key_Space, Qt::NoModifier);
-
-        app.postEvent(&viewer, keyPress);
-        app.postEvent(&viewer, keyRelease);
-        app.sendPostedEvents(0, 0);
-
-        int ret = app.exec();
-
-        //delete screenSaver;
-        delete ofonoSimIf;
-        return ret;
-    } else {
+    if (!ofonoSimIf->pinRequired()) {
         delete ofonoSimIf;
         exit(1);
     }
+
+    view->rootContext()->setContextProperty("ofonoSimIf", ofonoSimIf);
+    view->setSource(QUrl("qrc:/qml/main.qml"));
+
+    // QSystemScreenSaver is not available in MeeGo OBS
+    //QSystemScreenSaver *screenSaver = new QSystemScreenSaver();
+    //screenSaver->setScreenSaverInhibit();
+
+    view->showFullScreen();
+
+    // Send fake key presses to try to dismiss screensaver if it is alredy active.
+    // Don't delete events in this method
+    QKeyEvent* keyPress = new QKeyEvent( QEvent::KeyPress, Qt::Key_Space, Qt::NoModifier);
+    QKeyEvent* keyRelease = new QKeyEvent( QEvent::KeyRelease, Qt::Key_Space, Qt::NoModifier);
+
+    application->postEvent(view, keyPress);
+    application->postEvent(view, keyRelease);
+    application->sendPostedEvents(0, 0);
+
+    int ret = application->exec();
+
+    //delete screenSaver;
+    delete ofonoSimIf;
+    return ret;
 }
